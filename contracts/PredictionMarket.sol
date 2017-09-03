d7acdacae3adde9be809dd3ff36e20a292b15102
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-03 14:50:23 +0100

    added failing test to check that only admin indeed is allowed to set the question

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -3,8 +3,18 @@ pragma solidity ^0.4.11;
 
 contract PredictionMarket {
     address public admin;
+    string public question;
+
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
 
     function PredictionMarket(address _admin) {
         admin = _admin;
+    }
+
+    function setQuestion(string _question) public onlyAdmin {
+        question = _question;
     }
 }
--- test/prediction_market.js
+++ test/prediction_market.js
@@ -1,8 +1,62 @@
 const PredictionMarket = artifacts.require("./PredictionMarket.sol");
 
+function getTransactionReceiptMined(txHash, interval) {
+  const transactionReceiptAsync = function(resolve, reject) {
+    web3.eth.getTransactionReceipt(txHash, (error, receipt) => {
+      if (error) {
+        reject(error);
+      } else if (receipt == null) {
+        setTimeout(
+          () => transactionReceiptAsync(resolve, reject),
+          interval ? interval : 500);
+      } else {
+        resolve(receipt);
+      }
+    });
+  };
+
+  if (Array.isArray(txHash)) {
+    return Promise.all(txHash.map(
+      oneTxHash => getTransactionReceiptMined(oneTxHash, interval)));
+  } else if (typeof txHash === "string") {
+    return new Promise(transactionReceiptAsync);
+  } else {
+    throw new Error("Invalid Type: " + txHash);
+  }
+}
+
+function expectedExceptionPromise(action, gasToUse) {
+  return new Promise(function (resolve, reject) {
+    try {
+      resolve(action());
+    } catch(e) {
+      reject(e);
+    }
+  })
+  .then(function (txn) {
+    // https://gist.github.com/xavierlepretre/88682e871f4ad07be4534ae560692ee6
+    return getTransactionReceiptMined(txn);
+  })
+  .then(function (receipt) {
+    // We are in Geth
+    assert.equal(receipt.gasUsed, gasToUse, "should have used all the gas");
+  })
+  .catch(function (e) {
+    if (((e + "").indexOf("invalid opcode") > -1) || ((e + "").indexOf("out of gas") > -1)) {
+      // We are in TestRPC
+    } else if ((e + "").indexOf("please check your gas amount") > -1) {
+      // We are in Geth for a deployment
+    } else {
+      throw e;
+    }
+  });
+}
+
+
 contract("PredictionMarket", accounts => {
   const owner = accounts[0];
   const administrator = accounts[1];
+  const question = "Will climate change end the world by 2050?";
   let instance;
 
   beforeEach(() => {
@@ -23,5 +77,29 @@ contract("PredictionMarket", accounts => {
         "Administrator was not set!"
       );
     });
+  });
+
+  it("should allow the administrator add a question", () => {
+    return instance.setQuestion(
+      question,
+      {from: administrator})
+    .then(() => {
+      return instance.question();
+    })
+    .then(_question => {
+      assert.equal(
+        _question,
+        question,
+        "Administrator can set question"
+      );
+    });
+  });
+
+  it("should not let anyone but the administrator set the question", () => {
+    return expectedExceptionPromise(() => {
+      return instance.setQuestion(
+        question,
+        {from: owner});
+    }, 2000000);
   });
 });
