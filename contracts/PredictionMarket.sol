11eac2b09474f7a0af213d8101d2d24ba02c7d7d
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-03 15:58:58 +0100

    both admin and resolver added in constructor

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.11;
 
 contract PredictionMarket {
     address public admin;
+    address public resolver;
     string public question;
     mapping (address => Bet) public bets;
 
@@ -19,8 +20,9 @@ contract PredictionMarket {
     event LogQuestion(address indexed _sender, string _question);
     event LogBet(address indexed _sender, bool indexed _prediction);
 
-    function PredictionMarket(address _admin) {
+    function PredictionMarket(address _admin, address _resolver) {
         admin = _admin;
+        resolver = _resolver;
     }
 
     function setQuestion(string _question)
