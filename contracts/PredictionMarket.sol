1dd5ea965b701f9a0747b9c8e628194a6a244dc0
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-03 15:32:26 +0100

    allows users place bets by passing ether to the contract

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.4.11;
 contract PredictionMarket {
     address public admin;
     string public question;
+    mapping (address => Bet) public bets;
+
+    struct Bet {
+        bool prediction;
+        uint amount;
+    }
 
     modifier onlyAdmin {
         require(msg.sender == admin);
@@ -11,6 +17,7 @@ contract PredictionMarket {
     }
 
     event LogQuestion(address indexed _sender, string _question);
+    event LogBet(address indexed _sender, bool indexed _prediction);
 
     function PredictionMarket(address _admin) {
         admin = _admin;
@@ -23,6 +30,19 @@ contract PredictionMarket {
     {
         question = _question;
         LogQuestion(msg.sender, _question);
+        return true;
+    }
+
+    function place(bool _prediction)
+        public
+        payable
+        returns(bool)
+    {
+        require(msg.value > 0);
+        uint amount = msg.value;
+        Bet memory bet = Bet(_prediction, amount);
+        bets[msg.sender] = bet;
+        LogBet(msg.sender, _prediction);
         return true;
     }
 }
