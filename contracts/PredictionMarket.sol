79541262a9f2a10a1be183b5292f45426075c1d8
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-04 20:29:40 +0100

    only admins can add other admins

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -1,6 +1,26 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.15;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 
-contract PredictionMarket {
+contract PredictionMarket is Ownable {
+    mapping (address => bool) public isAdmin;
 
+    function PredictionMarket() {
+        isAdmin[msg.sender] = true;
+    }
+
+    modifier onlyAdmin {
+        require(isAdmin[msg.sender]);
+        _;
+    }
+
+    function addAdmin(address _admin)
+        public
+        onlyAdmin
+        returns(bool)
+    {
+        isAdmin[_admin] = true;
+        return true;
+    }
 }
