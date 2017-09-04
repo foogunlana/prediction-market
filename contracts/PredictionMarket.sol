860df8e8de630354cd2331d15df027a1973e8c0c
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-04 22:05:04 +0100

    yes no bets can be placed if equal to total amount sent

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -4,17 +4,22 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 
 contract PredictionMarket is Ownable {
+
     struct Question {
+        uint totalYesAmount;
+        uint totalNoAmount;
+    }
+    struct Bet {
         uint yesAmount;
         uint noAmount;
-        uint yesCount;
-        uint noCount;
     }
     mapping (address => bool) public isAdmin;
+    mapping (address => mapping (bytes32 => Bet)) public bets;
     mapping (bytes32 => Question) public questions;
 
     event LogAddAdmin(address _admin);
     event LogAddQuestion(address _admin, string _question);
+    event LogPlaceBet(address _user, string _question, uint _yesAmount, uint _noAmount);
 
     function PredictionMarket() {
         isAdmin[msg.sender] = true;
@@ -41,8 +46,22 @@ contract PredictionMarket is Ownable {
         returns(bool)
     {
         bytes32 questionHash = sha3(_question);
-        questions[questionHash] = Question(0, 0, 0, 0);
+        questions[questionHash] = Question(0, 0);
         LogAddQuestion(msg.sender, _question);
+        return true;
+    }
+
+    function placeBet(string _question, uint _yesAmount, uint _noAmount)
+        public
+        payable
+        returns(bool)
+    {
+        require(msg.value == _yesAmount + _noAmount);
+        bytes32 questionHash = sha3(_question);
+        bets[msg.sender][questionHash] = Bet(_yesAmount, _noAmount);
+        questions[questionHash].totalYesAmount += _yesAmount;
+        questions[questionHash].totalNoAmount += _noAmount;
+        LogPlaceBet(msg.sender, _question, _yesAmount, _noAmount);
         return true;
     }
 }
