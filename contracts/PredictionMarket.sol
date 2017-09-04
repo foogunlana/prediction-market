46316c8dd4786ff92d3313f0e921a6fbecbfeea1
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-04 21:06:15 +0100

    only admins can add questions

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -5,6 +5,10 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract PredictionMarket is Ownable {
     mapping (address => bool) public isAdmin;
+    mapping (bytes32 => string) public questions;
+
+    event LogAddAdmin(address _admin);
+    event LogAddQuestion(address _admin, bytes32 _questionHash);
 
     function PredictionMarket() {
         isAdmin[msg.sender] = true;
@@ -21,6 +25,18 @@ contract PredictionMarket is Ownable {
         returns(bool)
     {
         isAdmin[_admin] = true;
+        LogAddAdmin(_admin);
+        return true;
+    }
+
+    function addQuestion(string _question)
+        public
+        onlyAdmin
+        returns(bool)
+    {
+        bytes32 questionHash = sha3(_question);
+        questions[questionHash] = _question;
+        LogAddQuestion(msg.sender, questionHash);
         return true;
     }
 }
